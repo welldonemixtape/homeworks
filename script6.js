dcea61de6a87ee8349eb703612001d69add7961b
welldone <welldonemixtape@gmail.com>
2021-03-18 18:35:16 +0300

    cart

--- script6.js
+++ script6.js
@@ -1,80 +1,161 @@
-const cartItem = {
-    render(good) {
-        return `<div class="good">
-                    <div><b>Наименование</b>: ${good.product}</div>
-                    <div><b>Цена за шт.</b>: ${good.price}</div>
-                    <div><b>Количество</b>: ${good.num}</div>
-                    <div><b>Стоимость</b>: ${good.num * good.price}</div>
-                </div>`;
-    }
-}
+"use strict";
 
-const cart = {
-    cartListBlock: null,
-    cartButton: null,
-    addCart: null,
-    cartItem,
-    goods: [{
-        //     id: 007,
-        //     product: 'iMac',
-        //     price: 126000,
-        //     num: 0,
-        // },
-        // {
-        //     id: 022,
-        //     product: 'iPhone 12 256GB',
-        //     price: 89000,
-        //     num: 0,
-        // },
-        // {
-        //     id: 017,
-        //     product: 'AirPods',
-        //     price: 11900,
-        //     num: 0,
-        // },
-        // {
-        id: 089,
-        product: 'MacBook Pro',
-        price: 2990,
-        num: 0,
-    }, ],
+const catalog = {
+    catalogBlock: null,
+    cart: {},
+    list: [{
+            id_product: 123,
+            product: 'Apple iMac Pro',
+            price: 219000,
+        },
+        {
+            id_product: 456,
+            product: 'Apple iPhone 12 256GB',
+            price: 89000,
+        },
+        {
+            id_product: 245,
+            product: 'AirPods',
+            price: 11999,
+        }
+    ],
 
-    init() {
-        this.cartListBlock = document.querySelector('.cart-list');
-        this.cartButton = document.querySelector('.cart-btn');
-        this.cartButton.addEventListener('click', this.clearCart.bind(this));
-        this.addCart = document.querySelector('.add-cart');
-        this.addCart.addEventListener('click', this.addToCart.bind(this));
-
+    init(catalogBlockClass, cart) {
+        this.catalogBlock = document.querySelector(`.${catalogBlockClass}`);
+        this.cart = cart;
         this.render();
-    },
-
-    addToCart() {
-        return this.goods.num + 1;
+        this.addEventHandlers();
     },
 
     render() {
-        if (this.goods.length) {
-            this.goods.forEach(good => {
-                this.cartListBlock.insertAdjacentHTML('beforeend', this.cartItem.render(good));
-            });
-            this.cartListBlock.insertAdjacentHTML('beforeend', `В корзине <b>${this.goods.length}</b> позиция(й) стоимостью <b>${this.getCartPrice()}</b> рублей`);
+        if (this.getCatalogListLength() > 0) {
+            this.renderCatalogList();
         } else {
-            this.cartListBlock.textContent = 'Корзина пуста';
+            this.renderEmptyCatalog();
         }
     },
 
-    getCartPrice() {
-        return this.goods.reduce(function(price, good) {
-            return price + good.price * good.num;
-        }, 0);
+    addEventHandlers() {
+        this.catalogBlock.addEventListener('click', event => this.addToBasket(event));
     },
 
-    clearCart() {
+    addToBasket(event) {
+        if (!event.target.classList.contains('product__add-to-cart')) return;
+        const id_product = +event.target.dataset.id_product;
+        this.cart.addToBasket(id_product);
+    },
+
+    getCatalogListLength() {
+        return this.list.length;
+    },
+
+    renderCatalogList() {
+        this.catalogBlock.innerHTML = '';
+        this.list.forEach(item => {
+            this.catalogBlock.insertAdjacentHTML('beforeend', this.renderCatalogItem(item));
+        });
+    },
+
+    renderCatalogItem(item) {
+        return `<div class="product text-light bg-secondary rounded my-3 px-2 pt-3 w-25">
+                <h3>${item.product}</h3>
+                <p>${item.price} руб.</p>
+                <button class="product__add-to-cart btn btn-primary mb-3" data-id_product="${item.id_product}">В корзину</button>
+            </div>`;
+    },
+
+    renderEmptyCatalog() {
+        this.catalogBlock.innerHTML = '';
+        this.catalogBlock.insertAdjacentHTML('beforeend', `Каталог товаров пуст.`);
+    },
+};
+
+const cart = {
+    cartBlock: null,
+    clrCartButton: null,
+    catalogList: [],
+    goods: [{
+        id_product: 999,
+        product: 'Бесплатные EarPods',
+        price: 0,
+        num: 1,
+    }, ],
+
+    init(cartBlockClass, clrCartButton, catalogList) {
+        this.cartBlock = document.querySelector(`.${cartBlockClass}`);
+        this.clrCartButton = document.querySelector(`.${clrCartButton}`);
+        this.catalogList = catalogList;
+
+        this.addEventHandlers();
+        this.render();
+    },
+
+    addEventHandlers() {
+        this.clrCartButton.addEventListener('click', this.dropCart.bind(this));
+    },
+
+    dropCart() {
         this.goods = [];
         this.render();
     },
 
+    render() {
+        if (this.getCartGoodsLength() > 0) {
+            this.renderCartList();
+        } else {
+            this.renderEmptyCart();
+        }
+    },
+
+    findProductInCatalog(id_product) {
+        return this.catalogList.find(product => product.id_product === id_product);
+    },
+
+    addToBasket(id_product) {
+        const product = this.findProductInCatalog(id_product);
+
+        if (product) {
+            const findInBasket = this.goods.find(({ id_product }) => product.id_product === id_product);
+            if (findInBasket) {
+                findInBasket.num++;
+            } else {
+                this.goods.push({...product, num: 1 });
+            }
+            this.render();
+        } else {
+            alert('Ошибка добавления!');
+        }
+    },
+
+    getCartGoodsLength() {
+        return this.goods.length;
+    },
+
+    renderEmptyCart() {
+        this.cartBlock.innerHTML = '';
+        this.cartBlock.insertAdjacentHTML('beforeend', 'Корзина пуста.');
+    },
+
+    renderCartList() {
+        this.cartBlock.innerHTML = '';
+        this.goods.forEach(item => {
+            this.cartBlock.insertAdjacentHTML('beforeend', this.renderCartItem(item));
+        });
+    },
+
+    renderCartItem(item) {
+        return `<div class="bg-secondary rounded my-3 px-2 pt-3 pb-1 text-light w-25">
+                <h3>${item.product}</h3>
+                <p>${item.price * item.num} руб. (1 шт. = ${item.price} руб.)</p>
+                <p>${item.num} шт.</p>
+                </div>`
+
+    },
 };
 
-cart.init();
+/**
+ * Подключение каталога и корзины
+ */
+
+catalog.init('catalog', cart);
+cart.init('cart', 'clr-cart', catalog.list);
